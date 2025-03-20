9872ee67d8ab6e630d5f836d9e7a815281d964ff
Aditya Kashyap <adityaraikwar12789@gmail.com>
2025-03-20 14:20:11 +0530

    Added router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { Link, Route, Routes } from "react-router"
 import Home from "./pages/Home"
 
 function App() {
   return (
     <>
       <header className="bg-blue-400 flex justify-between items-center px-6 py-4 mb-4">
-        <h1 className="text-5xl font-bold">Shoppy</h1>
-        <p className="text-3xl font-bold">Cart</p>
+        <Link className="text-5xl font-bold" to="/">Shoppy</Link>
+        <Link className="text-5xl font-bold" to="/cart">Cart</Link>
       </header>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="cart" element={<Home />} />
+      </Routes>
+
+
     </>
   )
 }
